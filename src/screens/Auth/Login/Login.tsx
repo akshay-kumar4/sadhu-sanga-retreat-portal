36c4aa1601330bcb34138a1a6acd5b396366b0e1
Akshay Kumar <akshaykhigh8285@gmail.com>
2024-03-08 22:55:39 +0530

    Added auth screens

--- src/screens/Auth/Login/Login.tsx
+++ src/screens/Auth/Login/Login.tsx
@@ -1,5 +1,23 @@
+import LoginBanner from "../../../assets/login-banner.jpg";
+import Navbar from "@/screens/Home/components/Navbar";
+import Form from "./components/Form";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="flex flex-col md:flex-row min-h-screen">
+        <Form />
+        <div className="md:w-1/2 hidden md:block">
+          <img
+            src={LoginBanner}
+            alt="Placeholder"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/screens/Auth/Register/components/Form.tsx
@@ -0,0 +1,78 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Link } from "react-router-dom";
+import Google from "../../../../assets/google.svg";
+
+const Form = () => {
+  return (
+    <div className="md:w-1/2 bg-white md:flex md:justify-center md:items-center min-h-screen pt-60 w-full md:pt-0">
+      <div className="max-w-sm w-full px-6 py-8">
+        <h2 className="text-center font-semibold text-2xl mb-4 uppercase">
+          sign up
+        </h2>
+        <Button variant="outline" className="w-full py-2 px-4 rounded-md mb-4">
+          <img
+            src={Google}
+            height="20"
+            width="20"
+            className="mr-2"
+            alt="google-logo"
+          />
+          Login with Google
+        </Button>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Name
+            </label>
+            <Input placeholder="Your Name" type="text" id="name" name="name" />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Email
+            </label>
+            <Input
+              placeholder="Your Email"
+              type="email"
+              id="email"
+              name="email"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Password
+            </label>
+            <Input
+              placeholder="Password"
+              type="password"
+              id="password"
+              name="password"
+            />
+          </div>
+          <Button className="w-full" type="submit">
+            Login
+          </Button>
+        </form>
+        <div className="text-center mt-4">
+          <p>
+            Already a user?
+            <Link to="/portal/login">
+              <span className="text-blue-500 cursor-pointer ml-2">Sign In</span>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
